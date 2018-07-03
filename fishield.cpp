c3d4a9943b17e3561f99d07a4963af1a5792502a
LLipter <katsudonn@163.com>
2018-07-03 22:08:06 +0800

    first commit

---
+++ fishield.cpp
@@ -0,0 +1,6 @@
+#include    "fishield.h"
+#include    <iostream>
+
+void    foo(){
+    std::cout << "hello world" << std::endl;
+}
---
+++ fishield.h
@@ -0,0 +1,9 @@
+#ifndef FISHIELD_H
+#define FISHIELD_H
+
+void    foo();
+
+
+
+
+#endif
