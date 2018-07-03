6facb57042fe79240d047833a561d8e0e066d64f
LLipter <katsudonn@163.com>
2018-07-04 03:59:58 +0800

    add some macro

--- fs_callback.h
+++ fs_callback.h
@@ -3,6 +3,9 @@
 
 #include <map>
 
+#define FS_EVENT_SYSTEM     "event.system"
+#define FS_EVENT_PROCESS    "event.process"
+
 typedef int (*secft_func_ptr)(void* user_data, std::map<std::string,std::string> msg);
 
 struct fs_callback{
