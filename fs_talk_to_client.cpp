039bcc5ab8688f722eb2df4f2f317ea8adba6f92
LLipter <katsudonn@163.com>
2018-07-04 20:42:58 +0800

    some member variables

--- fs_talk_to_client.cpp
+++ fs_talk_to_client.cpp
@@ -1,11 +1,25 @@
 #include "fs_talk_to_client.h"
 
-boost::asio::io_service service;
+extern boost::asio::io_service service;
 
 fs_talk_to_client::fs_talk_to_client() : _sock(service){
     memset(data_buffer,0,sizeof(data_buffer));
+    is_login = false;
 }
 
 boost::asio::ip::tcp::socket& fs_talk_to_client::sock(){
     return _sock;
+}
+
+void fs_talk_to_client::login(){
+    is_login = true;
+}
+
+std::string fs_talk_to_client::username(){
+    return _username;
+}
+
+void fs_talk_to_client::stop(){
+    boost::system::error_code err;
+    _sock.close(err);
 }
--- fs_talk_to_client.h
+++ fs_talk_to_client.h
@@ -4,6 +4,7 @@
 #include "fs_config.h"
 #include <boost/asio.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
 class fs_talk_to_client :
         public boost::enable_shared_from_this<fs_talk_to_client>,
@@ -11,9 +12,18 @@ class fs_talk_to_client :
 {
 public:
     fs_talk_to_client();
+    boost::asio::ip::tcp::socket& sock();
+    void login();
+    std::string username();
+    void stop();
 private:
+    // member varibales
     boost::asio::ip::tcp::socket _sock;
     char data_buffer[BUFFER_SIZE];
+    bool is_login;
+    std::string _username;
+    // member functions
+    void read_request();
 };
 
 #endif // FS_TALK_TO_CLIENT_H
