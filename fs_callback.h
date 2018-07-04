770ebf51a0e0316367ec84dd8647523fb72bc4e3
LLipter <katsudonn@163.com>
2018-07-04 11:10:40 +0800

    add enum fs_process_event

--- fs_callback.h
+++ fs_callback.h
@@ -6,6 +6,17 @@
 #define FS_EVENT_SYSTEM     "event.system"
 #define FS_EVENT_PROCESS    "event.process"
 
+typedef enum fs_process_event {
+    sec_process_upload = 1,
+    sec_process_download,
+    sec_process_paused,
+    sec_process_canceled,
+    sec_process_resume,
+    sec_process_failed,
+    sec_process_uploaded,
+    sec_process_downloaded,
+} fs_process_event;
+
 typedef int (*fs_func_ptr)(void* user_data, std::map<std::string,std::string> msg);
 
 struct fs_callback{
