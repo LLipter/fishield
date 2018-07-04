040da9beaa13db1bb31fea48c1c5b2014b7b316a
LLipter <katsudonn@163.com>
2018-07-04 11:12:05 +0800

    modify name of fs_process_event

--- fs_callback.h
+++ fs_callback.h
@@ -7,14 +7,14 @@
 #define FS_EVENT_PROCESS    "event.process"
 
 typedef enum fs_process_event {
-    sec_process_upload = 1,
-    sec_process_download,
-    sec_process_paused,
-    sec_process_canceled,
-    sec_process_resume,
-    sec_process_failed,
-    sec_process_uploaded,
-    sec_process_downloaded,
+    fs_process_upload = 1,
+    fs_process_download,
+    fs_process_paused,
+    fs_process_canceled,
+    fs_process_resume,
+    fs_process_failed,
+    fs_process_uploaded,
+    fs_process_downloaded,
 } fs_process_event;
 
 typedef int (*fs_func_ptr)(void* user_data, std::map<std::string,std::string> msg);
