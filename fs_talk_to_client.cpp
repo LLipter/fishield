1af4ac58efc96c0a46e2dfa2e928a8900d7baca0
LLipter <katsudonn@163.com>
2018-07-04 20:51:58 +0800

    add error handling

--- fs_talk_to_client.cpp
+++ fs_talk_to_client.cpp
@@ -1,4 +1,5 @@
 #include "fs_talk_to_client.h"
+#include "fs_error.h"
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
 extern boost::asio::io_service service;
@@ -27,38 +28,37 @@ void fs_talk_to_client::stop(){
 }
 
 void fs_talk_to_client::read_request() {
-        char buff[4] = {0};
-        already_read = 0;
-        if ( _sock.available()){
-            (void)_sock.receive(boost::asio::buffer(buff, 4), boost::asio::ip::tcp::socket::message_peek );
-            google::protobuf::uint32 size;
-            google::protobuf::io::ArrayInputStream ais_hdr(buff,4);
-            google::protobuf::io::CodedInputStream coded_input_hdr(&ais_hdr);
-            coded_input_hdr.ReadVarint32(&size);//Decode the HDR and get the size
+    char buff[4] = {0};
+    already_read = 0;
+    if ( _sock.available()){
+        (void)_sock.receive(boost::asio::buffer(buff, 4), boost::asio::ip::tcp::socket::message_peek );
+        google::protobuf::uint32 size;
+        google::protobuf::io::ArrayInputStream ais_hdr(buff,4);
+        google::protobuf::io::CodedInputStream coded_input_hdr(&ais_hdr);
+        coded_input_hdr.ReadVarint32(&size);//Decode the HDR and get the size
 
-            already_read = _sock.read_some(boost::asio::buffer(data_buffer, 4+size));
-            if(already_read <= 0) {
-                return;
-            }
-            google::protobuf::io::ArrayInputStream ais(data_buffer,size+4);
-            google::protobuf::io::CodedInputStream coded_input(&ais);
-            //Read an unsigned integer with Varint encoding, truncating to 32 bits.
-            coded_input.ReadVarint32(&size);
-            //After the message's length is read, PushLimit() is used to prevent the CodedInputStream
-            //from reading beyond that length.Limits are used when parsing length-delimited
-            //embedded messages
-            google::protobuf::io::CodedInputStream::Limit msgLimit = coded_input.PushLimit(size);
-            //De-Serialize
-
-            fs::proto::packet::Request request;
-            request.ParseFromCodedStream(&coded_input);
-            //Once the embedded message has been parsed, PopLimit() is called to undo the limit
-            coded_input.PopLimit(msgLimit);
-            std::cout << request.packet().user_name();
-            std::cout << request.packet().token();
-            client_req = request;
-            //Print the message
+        already_read = _sock.read_some(boost::asio::buffer(data_buffer, 4+size));
+        if(already_read <= 0) {
+            return;
         }
+        google::protobuf::io::ArrayInputStream ais(data_buffer,size+4);
+        google::protobuf::io::CodedInputStream coded_input(&ais);
+        //Read an unsigned integer with Varint encoding, truncating to 32 bits.
+        coded_input.ReadVarint32(&size);
+        //After the message's length is read, PushLimit() is used to prevent the CodedInputStream
+        //from reading beyond that length.Limits are used when parsing length-delimited
+        //embedded messages
+        google::protobuf::io::CodedInputStream::Limit msgLimit = coded_input.PushLimit(size);
+        //De-Serialize
 
-
-    }
+        fs::proto::packet::Request request;
+        request.ParseFromCodedStream(&coded_input);
+        //Once the embedded message has been parsed, PopLimit() is called to undo the limit
+        coded_input.PopLimit(msgLimit);
+        std::cout << request.packet().user_name();
+        std::cout << request.packet().token();
+        client_req = request;
+        //Print the message
+    }else
+        err_quit("fs_talk_to_client::read_request() error - _sock is not available");
+}
